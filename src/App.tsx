7f6ea38e4146917b33f8557a791cab935dfa101c
Roger-iin <rogersimoes@hotmail.com>
2026-07-03 17:13:48 -0300

    feat: Cria a rota protegida /dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { AuthProvider } from './contexts/AuthContext'
 import { Login } from './pages/Login'
+import { Dashboard } from './pages/Dashboard'
+import { PrivateRoute } from './components/PRivateRoute'
 
 function App() {
   return (
@@ -9,6 +11,14 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path='/' element={<Login />}/>
+          <Route 
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
