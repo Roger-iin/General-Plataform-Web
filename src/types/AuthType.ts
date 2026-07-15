3741e8a90279ee7860d3b6833b1f02c628655a54
Roger-iin <rogersimoes@hotmail.com>
2026-07-15 15:22:49 -0300

    feat: Adiciona isLoading no AuthContextData e corrige typo no SignUpResponse

--- src/types/AuthType.ts
+++ src/types/AuthType.ts
@@ -5,13 +5,14 @@ export interface User {
 
 export interface AuthContextData {
     user: User | null;
+    isLoading: boolean;
     isAuthenticated: boolean;
     signIn: (email: string, password: string) => Promise<void>;
     signOut: () => void;
     signUp: (name: string, email: string, password: string) => Promise<void>;
 }
 
-export interface signUpResponse{
+export interface SignUpResponse{
     id: string,
     name: string,
     email: string,
